d0553316b67d1d25bae4ab138cb3f1966625f08c
Danilo <dznery@gmail.com>
2018-05-07 01:44:31 -0300

    Implement SPFA algorithm
    It's a lot faster. All algorithms outputting different paths,
    and all are (apparently) valid. DON'T FORGET TO TEST THEM
    MANUALLY!!!

--- project1/bf-arbitrate.r
+++ project1/bf-arbitrate.r
@@ -91,9 +91,9 @@ bf.find <- function(graph, summary) {
     if (NROW(cycle) > 0) {
         rates = as.numeric(as.character(summary[,3]))
         profit = bf.mult(edges, rates, cycle)
-        cat("profit:", profit, "\n")
+        cat("possible profit:", profit, "\n")
     } else {
-        cat("no negative cycles found")
+        cat("no arbitrage opportunity detected :(\n")
     }
 
     ret = list()
@@ -102,6 +102,7 @@ bf.find <- function(graph, summary) {
     ret$prevs = prevs
     ret$cycle = cycle
     ret$profit = profit
+
     return(ret)
 }
 
@@ -116,31 +117,91 @@ bf.spfa <- function(graph, summary) {
     dists = rep(Inf, NROW(nodes))
     prevs = rep(NULL, NROW(nodes))
 
+    # worst case iter count
+    max_iters = NROW(nodes) - 1
+
     # distance from source is zero
     # source is always the artificial node
     dists[1] = 0
 
-    # add source to queue
+    # Q behaves as a FIFO queue
+    # ...also, add source to it
     Q = c(1)
 
     while (NROW(Q) > 0) {
 
-        # randomly retract a node the from queue
-        node = as.numeric(sample(Q, NROW(Q), 1))
+        # get front
+        node = Q[1]
         Q = Q[-which(Q==node)]
 
-        # separate it's neighbors
-        neighbors.edges = edges[which(edges[1,]==nodes[node]),]
-        neighbors.nodes = neighbors.edges[,2]
+        cat("  inspecting node:", node, "\n")
 
-        for (i in 1:NROW(neighbors.nodes)) {
+        # neighbor node edge ids
+        edge.ids = which(edges[,1]==nodes[node])
 
-            # TODO HERE
-            # TODO HERE
-            # TODO HERE
-            # TODO HERE
-            # TODO HERE
+        for (i in 1:NROW(edge.ids)) {
+
+            # distance inbetween
+            distance = weights[edge.ids[i]]
+
+            # neighbor node number
+            neighbor = which(nodes==edges[edge.ids[i], 2])
+
+            if (dists[node] + distance < dists[neighbor]) {
+                dists[neighbor] = dists[node] + distance
+                prevs[neighbor] = node
+
+                # add to queue (only once)
+                if (sum(Q==neighbor) == 0) {
+                    Q = c(Q, neighbor)
+                }
+            }
+
         }
 
+        # necessary for worst case
+        max_iters = max_iters - 1
+        if (max_iters == 0) {
+            break
+        }
     }
+
+    # find negative cycle
+    cycle = NULL
+    profit = NULL
+    for (i in 1:NROW(edges)) {
+
+        u = which(nodes==edges[i, 1]) # node
+        v = which(nodes==edges[i, 2]) # neighbor
+
+        # only possible in the presence of negatie cycles
+        if (dists[u] + weights[i] < dists[v]) {
+
+            # cycle path
+            cat("the following arbitrage is possible:\n")
+            cycle = bf.path(nodes, prevs, edges[i,1])
+            print(cycle)
+
+            # cycle mult
+            rates = as.numeric(as.character(summary[,3]))
+            profit = bf.mult(edges, rates, cycle)
+            cat("possible profit:", profit, "\n")
+
+            break
+        }
+    }
+
+    # case of "failure"
+    if (NROW(cycle) == 0) {
+        cat("no arbitrage opportunity detected :(")
+    }
+
+    ret = list()
+    ret$nodes = nodes
+    ret$dists = dists
+    ret$prevs = prevs
+    ret$cycle = cycle
+    ret$profit = profit
+
+    return(ret)
 }
