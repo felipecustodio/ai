717c816872a4c1010b50df6a263ccaa56135e61a
Danilo <dznery@gmail.com>
2018-05-08 01:26:22 -0300

    Implement btx scraper, include more btx datasets
    STILL NEEDS TESTING!!!
    
    WHAT?
    
    EVERYTHING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- project1/scrape-util.r
+++ project1/scrape-util.r
@@ -1,47 +1,34 @@
-# TODO TOTALLY REWRITE THIS
 scrape.btx <- function() {
 
     require(jsonlite)
 
-    # scrape nodes
-    node_data.raw = readLines("https://bittrex.com/api/v1.1/public/getcurrencies")
-    node_data.json = jsonlite::fromJSON(node_data.raw)
+    # scrape raw data
+    rawdata = readLines("https://bittrex.com/api/v1.1/public/getmarketsummaries")
+    edges = jsonlite::fromJSON(rawdata)
+    print("requests done")
 
-    # scrape edges
-    edge_data.raw = readLines("https://bittrex.com/api/v1.1/public/getmarketsummaries")
-    edge_data.json = jsonlite::fromJSON(edge_data.raw)
+    # parsed edge data
+    market.names = edges$result$MarketName
+    pairs = apply(as.matrix(market.names), 1, function(row) {
+        # original data is a nested list
+        strsplit(row, "-")[[1]]
+    })
 
-    print("requests done, getting data ready...")
+    # so it becomes and N by 2 matrix
+    pairs = as.matrix(t(pairs))
 
-    # nodes: indices
-    nodes = node_data.json$result$Currency
-    nodes = cbind(nodes, seq(1, NROW(nodes)))
+    # bittrex returns flipped coin pairs
+    reverse.pairs = cbind(pairs[,2], pairs[,1])
 
-    # edges: indices
-    market_names = edge_data.json$result$MarketName
-    parsed_names = apply(as.matrix(market_names), 1, function(row) {
-                             strsplit(row, "-")[[1]] # this crap returns a nested list
-})
-    parsed_names = t(parsed_names) # so it becomes and N by 2 matrix
-    parsed_ids = cbind(rep(0, NROW(parsed_names)), rep(0, NROW(parsed_names)))
+    bids = as.numeric(as.character(edges$result$Bid))
+    asks = 1/as.numeric(as.character(edges$result$Ask))
+    asks[which(asks==Inf)] = 0
 
-    for (i in 1:NROW(nodes)) {
-        currency = nodes[i, 1]
-        currency_id = as.numeric(nodes[i, 2])
-        parsed_ids[which(parsed_names==currency)] = currency_id
-    }
+    bids.pairs = cbind(reverse.pairs, bids)
+    asks.pairs = cbind(pairs, asks)
 
-    # edges: weights
-    asks = 1/as.numeric(as.character(edge_data.json$result$Ask))
-    bids = edge_data.json$result$Bid
+    graph = rbind(asks.pairs, bids.pairs)
 
-    parsed_ids_ask = as.matrix(cbind(parsed_ids, asks))
-    parsed_ids_bid = as.matrix(cbind(parsed_ids[,2], parsed_ids[,1], bids))
-    edges = rbind(parsed_ids_ask, parsed_ids_bid)
-
-    ret = list()
-    ret$nodes = data.frame(nodes)
-    ret$edges = data.frame(edges)
-
-    return(ret)
+    # return
+    return(graph)
 }
